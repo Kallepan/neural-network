17245e09bfd659bb88d051702527aad4362fc5b6
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2023-10-18 20:38:20 +0000

    cleanup: code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
-mod lib;
+use lib::{neural_network::Network, activation::SIGMOID};
+
+pub mod lib;
+
+// Sample Case: XOR
+// 0, 0 -> 0
+// 0, 1 -> 1
+// 1, 0 -> 1
+// 1, 1 -> 0
 
 fn main() {
-    println!("Hello, world!");
+    let inputs = vec![
+        vec![0.0, 0.0],
+        vec![0.0, 1.0],
+        vec![1.0, 0.0],
+        vec![1.0, 1.0],
+    ];
+
+    let targets = vec![
+        vec![0.0],
+        vec![1.0],
+        vec![1.0],
+        vec![0.0],
+    ];
+    
+    let mut network = Network::new(
+        vec![2,3,1],
+        0.5,
+        SIGMOID
+    );
+
+    network.train(inputs, targets, 10000);
+
+    println!("0, 0 -> {:?}", network.feed_forward(vec![0.0, 0.0]));
+    println!("0, 1 -> {:?}", network.feed_forward(vec![0.0, 1.0]));
+    println!("1, 0 -> {:?}", network.feed_forward(vec![1.0, 0.0]));
+    println!("1, 1 -> {:?}", network.feed_forward(vec![1.0, 1.0]));
 }
